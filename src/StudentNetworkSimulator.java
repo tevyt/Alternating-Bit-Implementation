0f907dc943070b3aa6ca25711a6063f45f186749
Travis Smith <travissmith94@hotmail.com>
2015-10-03 17:44:20 -0500

    Implemented Sender flow without corruption checking

--- src/StudentNetworkSimulator.java
+++ src/StudentNetworkSimulator.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class StudentNetworkSimulator extends NetworkSimulator
 {
     /*
@@ -78,12 +82,15 @@ public class StudentNetworkSimulator extends NetworkSimulator
      *
      */
 
+    SenderState senderState; //Class containing Senders information.
+
     // Add any necessary class variables here.  Remember, you cannot use
     // these variables to send messages error free!  They can only hold
     // state information for A or B.
     // Also add any necessary methods (e.g. checksum of a String)
 
     // This is the constructor.  Don't touch!
+
     public StudentNetworkSimulator(int numMessages,
                                    double loss,
                                    double corrupt,
@@ -100,18 +107,35 @@ public class StudentNetworkSimulator extends NetworkSimulator
     // the receiving upper layer.
     protected void aOutput(Message message)
     {
-	  Packet p = new Packet(0,0,0,message.getData());
-	  toLayer3(0, p);
-	  startTimer(0,5);
+      senderState.addPacketToQueue(message);
+      if(!senderState.hasUnacknowledgedPackets()) {
+          Packet p = senderState.preparePacket();
+          toLayer3(0, p);
+          startTimer(0, 5);
+      }
     }
-    
+
     // This routine will be called whenever a packet sent from the B-side 
     // (i.e. as a result of a toLayer3() being done by a B-side procedure)
     // arrives at the A-side.  "packet" is the (possibly corrupted) packet
     // sent from the B-side.
     protected void aInput(Packet packet)
     {
-	  System.out.println("Received ACK");
+      Packet nextPacket;
+      System.out.println("Received ACK");
+      //If the packet received does not have the expected ACK , retransmit the last unacknowledged ACK.
+      if(!senderState.isExpectedPacket(packet)){
+         nextPacket = senderState.getLastUnacknowledgedPacket();
+         toLayer3(0 , nextPacket);
+         startTimer(0 , 5);
+      }else if((nextPacket = senderState.nextPacket()) != null){ //If there are more packets waiting to be transmitted send the next one.
+         toLayer3(0 , nextPacket);
+          startTimer(0 , 5);
+         senderState.setNextExpectedAcknowledgementNumber();
+      }else{ //Otherwise stop the timer and indicate that there are no currently unacknowledged packets.
+         stopTimer(0);
+         senderState.setNoUnacknowledgedPackets();
+      }
     }
     
     // This routine will be called when A's timer expires (thus generating a 
@@ -120,6 +144,9 @@ public class StudentNetworkSimulator extends NetworkSimulator
     // for how the timer is started and stopped. 
     protected void aTimerInterrupt()
     {
+      Packet retransmissionPacket = senderState.packet;
+      toLayer3(0 , retransmissionPacket);
+      startTimer(0 , 5);
 	  System.out.println("Timer");
     }
     
@@ -129,6 +156,7 @@ public class StudentNetworkSimulator extends NetworkSimulator
     // of entity A).
     protected void aInit()
     {
+        senderState = new SenderState(); //Initialize Sender state values.
     }
     
     // This routine will be called whenever a packet sent from the B-side 
@@ -155,4 +183,88 @@ public class StudentNetworkSimulator extends NetworkSimulator
 	  S.runSimulator();
 
 	}
+
+    //Inner class to store state that the sender must maintain.
+    static class SenderState{
+        private final static int NO_UNACKNOWLEDGED_PACKETS = -1;
+        private int sequenceNumber;
+        private int acknowledgementNumber; //indicates the acknowledgement number that the sender expects.
+        private Queue<Packet> packets; //Store unsent packets from Layer 5.
+        private Packet packet; //A copy of the last unacknowledged packet for retransmission.
+
+        public SenderState(){
+            this.sequenceNumber = 0;
+            this.acknowledgementNumber = NO_UNACKNOWLEDGED_PACKETS;//-1 indicates there are no
+            this.packets = new LinkedList<>();
+        }
+
+        //Retrieve the ACK number that the sender awaits.
+        public int getAwaitedAcknowledgementNumber() {
+            return acknowledgementNumber;
+        }
+
+        //Set AcknowledgementNumber to the sequence number of a packet.
+        public void setNextExpectedAcknowledgementNumber() {
+            this.acknowledgementNumber++;
+        }
+
+        //Return a copy of the last unacknowledged packet.
+        public Packet getLastUnacknowledgedPacket(){
+            return packet;
+        }
+
+        public void setPacket(Packet packet) {
+            this.packet = new Packet(packet);
+        }
+
+        public void addPacketToQueue(Message message){
+            int sequenceNumber = ++this.sequenceNumber;
+            int checkSum = computeCheckSum(sequenceNumber , 0 , message.getData());
+            Packet packet = new Packet(sequenceNumber , 0 , checkSum, message.getData());
+            packets.add(packet);
+        }
+
+        public boolean hasUnacknowledgedPackets(){
+            return acknowledgementNumber != NO_UNACKNOWLEDGED_PACKETS;
+        }
+
+        public Packet preparePacket(){
+           Packet packet = packets.poll(); //Get the next packet in the Queue
+           if(packet != null){
+              setPacket(packet);
+              this.acknowledgementNumber = packet.getSeqnum();//set expected acknowledgment number to the sequence number of the sent packet.
+           }
+           return packet;
+        }
+
+        //Checks if the packet received is the one expected
+        public boolean isExpectedPacket(Packet packet){
+            return  packet.getAcknum() == acknowledgementNumber;
+        }
+
+        public Packet nextPacket(){
+            return packets.poll();
+        }
+
+        public void setNoUnacknowledgedPackets(){
+           this.acknowledgementNumber = NO_UNACKNOWLEDGED_PACKETS;
+        }
+    }
+
+    static class receiverState{
+        public final
+    }
+
+    private static int computeCheckSum(int sequenceNumber , int acknowledgementNumber , String message){
+        int sum = sequenceNumber + acknowledgementNumber + characterSum(message.toCharArray());
+        return sum;
+    }
+
+    private static int characterSum(char[] characters){
+        int sum = 0;
+        for(int current: characters){
+            sum += current;
+        }
+        return sum;
+    }
 }
